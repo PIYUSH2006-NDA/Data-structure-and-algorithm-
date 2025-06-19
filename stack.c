bb53223a74a84779f3290fa3df86f80a417ba953
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2025-06-19 15:08:05 +0530

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack {
+    int size;
+    int top;
+    int *arr;
+};
+
+int isempty(struct stack *ptr) {
+    return (ptr->top == -1);
+}
+
+int isfull(struct stack *ptr) {
+    return ptr->top == (ptr->size - 1);
+}
+
+void push(struct stack *ptr, int val) {
+    if (isfull(ptr)) {
+        printf("stack is full\n");
+    } else {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+int pop(struct stack *ptr) {
+    if (isempty(ptr)) {
+        printf("stack is empty\n");
+        return -1;
+    } else {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+void display(struct stack *ptr) {
+    if (isempty(ptr)) {
+        printf("Stack is empty.\n");
+    } else {
+        printf("Stack elements (top to bottom):\n");
+        for (int i = ptr->top; i >= 0; i--) {
+            printf("%d\n", ptr->arr[i]);
+        }
+    }
+}
+
+int main() {
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    
+    s->size = 20;
+    s->top = -1; // Initialize as empty stack
+    s->arr = (int *)malloc(s->size * sizeof(int));
+    
+    push(s, 5);
+    push(s, 15);
+    push(s, 52);
+
+    display(s);
+
+    printf("popped element is %d\n", pop(s));
+    display(s);
+
+    printf("is stack empty? %s\n", isempty(s) ? "yes" : "no");
+    printf("is stack full? %s\n", isfull(s) ? "yes" : "no");
+
+    free(s->arr);
+    free(s);
+    return 0;
+}
